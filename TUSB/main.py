1ab45e0cf891d54a700bf20e295f736285976b5c
XXmmRR <73822451+XXmmRR@users.noreply.github.com>
2020-11-08 09:51:27 +0300

    Add files via upload

---
+++ TUSB/main.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+import sys
+import time
+
+def picture():
+    print('████████╗██╗░░░██╗░██████╗██████╗░')
+    time.sleep(0.5)
+    print('╚══██╔══╝██║░░░██║██╔════╝██╔══██╗')
+    time.sleep(0.5)
+    print('░░░██║░░░██║░░░██║╚█████╗░██████╦╝')
+    time.sleep(0.5)
+    print('░░░██║░░░██║░░░██║░╚═══██╗██╔══██╗')
+    time.sleep(0.5)
+    print('░░░██║░░░╚██████╔╝██████╔╝██████╦╝')
+    time.sleep(0.5)
+    print('░░░╚═╝░░░░╚═════╝░╚═════╝░╚═════╝░  by XXmmRR')
+picture()
+
+print("Нажми Enter чтобы запустить...")
+input()
+
+while (True):
+    process = subprocess.Popen([sys.executable, "Прототип_TUSB.py"])
+    process.wait()
