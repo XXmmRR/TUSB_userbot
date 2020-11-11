965fdc8115b2b4f89a89d0433094ff623f00d18d
XXmmRR <73822451+XXmmRR@users.noreply.github.com>
2020-11-11 10:15:29 +0300

    Add files via upload
    installator

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bir/bash
+sudo apt upgrade -y
+sudo apt update -y
+sudo apt install python -y
+sudo apt install libjpeg-turbo libcrypt ndk-sysroot clang zlib -y
+sudo apt install libjpeg-turbo -y
+cd TUSB_userbot
+cd TUSB
+pip3 install -r requerements.txt
