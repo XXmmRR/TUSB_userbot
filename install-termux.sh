4eeb800a13be8ed68d4fa0abc2c2b0dddaff3f49
XXmmRR <73822451+XXmmRR@users.noreply.github.com>
2020-11-11 10:40:30 +0300

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -1,5 +1,4 @@
 #!/bir/bash
-pkg upgrade -y
 pkg update -y
 pkg install python -y
 pkg install libjpeg-turbo libcrypt ndk-sysroot clang zlib -y
@@ -7,6 +6,5 @@ pkg install libjpeg-turbo -y
 LDFLAGS=" -lm" pip3 install pillow
 pkg install nano
 pkg install vim-python
-cd TUSB_userbot
 cd TUSB
 pip3 install -r requerements.txt
