8d9e447d0443525e8135074b25edc88c7ff31a17
XXmmRR <73822451+XXmmRR@users.noreply.github.com>
2020-11-11 12:10:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,4 @@
 sudo apt install python -y
 sudo apt install libjpeg-turbo libcrypt ndk-sysroot clang zlib -y
 sudo apt install libjpeg-turbo -y
-cd TUSB_userbot
-cd TUSB
 pip3 install -r requerements.txt
