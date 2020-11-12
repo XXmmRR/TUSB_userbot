91d94f4e2dc1df2ae008cca1c53610b9bb74fbd5
XXmmRR <73822451+XXmmRR@users.noreply.github.com>
2020-11-12 20:56:50 +0300

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -1,8 +1,9 @@
 #!/bir/bash
+pkg install subversion -y
 pkg install python -y
 pkg install libjpeg-turbo libcrypt ndk-sysroot clang zlib -y
 pkg install libjpeg-turbo -y
 LDFLAGS=" -lm" pip3 install pillow
-pkg install nano
-pkg install vim
+pkg install nano -y
+pkg install vim -y
 pip3 install -r requirements.txt
